85c43615933f8267d4b913bc4bab06cf7e196ee6
xam4lor <xam4lor@gmail.com>
2021-08-13 15:21:30 +0200

    Pushed default rendering structure

--- app/render/CoreAppRenderer.cpp
+++ app/render/CoreAppRenderer.cpp
@@ -8,17 +8,14 @@ void CoreAppRenderer::initialize() {
             {0, "Depth attachment", RenderPassAttachment::Type::Depth},
             // Mesh image
             {1, "Mesh image", RenderPassAttachment::Type::Image, VK_FORMAT_R8G8B8A8_UNORM},
-            // Filtered image
-            {2, "Filter image", RenderPassAttachment::Type::Image, VK_FORMAT_R8G8B8A8_UNORM},
             // Render to swapchain
-            {3, "Swapchain attachment", RenderPassAttachment::Type::Swapchain, VK_FORMAT_UNDEFINED, Color(0.1f, 0.105f, 0.11f)}
+            {2, "Swapchain attachment", RenderPassAttachment::Type::Swapchain, VK_FORMAT_UNDEFINED, Color(0.1f, 0.105f, 0.11f)}
 	};
 	// List of every sub-passes in the render pass
 	std::vector<RenderSubpassType> renderPassSubpasses0 = {
 			{0, { 0, 1 }},
 			{1, { 2 }, { 1 }},
-			{2, { 3 }, { 2 }},
-			{3, { 3 }}
+			{2, { 2 }}
 	};
 
 	// Creates the render pass 0
@@ -30,9 +27,10 @@ void CoreAppRenderer::start() {
 	// Mesh sub-renderer
 	this->addSubrenderer<MeshSubrenderer>({0, 0});
 
-	this->addSubrenderer<InvertFilter>({0, 1}, 1);
-	this->addSubrenderer<DefaultFilter>({0, 2}, 2);
+	// Filter
+	// this->addSubrenderer<InvertFilter>({0, 1}, 1);
+	this->addSubrenderer<DefaultFilter>({0, 1}, 1);
 
 	// Gui sub-renderer
-	this->addSubrenderer<GUISubrenderer>({0, 3});
+	this->addSubrenderer<GUISubrenderer>({0, 2});
 }
